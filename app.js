742418d19e158f8230de7c945c271fe3d2af7efb
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-02-07 19:36:13 +0300

    hatasız calısan 2.commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs= require('ejs');
+
+ // TEMPLATES ENGINE
+ app.set('view engine', 'ejs')
+
+ //MIDDLEWARES
+ app.use(express.static("public"));
+
+ 
+app.get('/', (req,res)=>{
+res.render("index");
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`basarıyla ${port}'a baglanildi`)
+})

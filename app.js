45894093b5bbf46c073889b364bb70e03eb0c420
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-02-07 20:21:44 +0300

    ejs ve partials yüklendi

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const ejs= require('ejs');
+const path = require("path");
 
  // TEMPLATES ENGINE
- app.set('view engine', 'ejs')
+ app.set('view engine', 'ejs');
 
  //MIDDLEWARES
  app.use(express.static("public"));
@@ -16,4 +17,4 @@ res.render("index");
 const port = 3000;
 app.listen(port, ()=>{
     console.log(`basarıyla ${port}'a baglanildi`)
-})
+});
